73233ab065796ee6480f89d46aa47eeed4b4cef7
Patrick Mutwiri <patwiri@gmail.com>
2018-08-16 09:37:47 +0300

    show consumables on dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -78,7 +78,7 @@
     </div>
   </div><!-- ./col -->
 
-  <div class="hidden col-lg-4 col-xs-6">
+  <div class="col-lg-4 col-xs-6">
     <!-- small box -->
     <div class="small-box bg-purple">
       <div class="inner">
